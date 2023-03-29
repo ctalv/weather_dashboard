34cdc9f007d2841a400ebbcc1ce268154603eeba
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-29 15:05:13 -0500

    added API keys to JS

---
+++ script.js
@@ -0,0 +1,5 @@
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+var urlZip = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=a62c7d10877c661b208fffa0f58b2658'
+var urlLatLon = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=a62c7d10877c661b208fffa0f58b2658'
